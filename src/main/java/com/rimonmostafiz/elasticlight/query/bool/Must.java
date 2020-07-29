02ea11aa9bb409d10a7c3a1b758e17713d3ad597
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-07-29 14:31:21 +0600

    Update Must Query

--- src/main/java/com/rimonmostafiz/elasticlight/query/bool/Must.java
+++ src/main/java/com/rimonmostafiz/elasticlight/query/bool/Must.java
@@ -1,6 +1,8 @@
 package com.rimonmostafiz.elasticlight.query.bool;
 
+import com.rimonmostafiz.elasticlight.query.builder.TermQueryBuilder;
 import com.rimonmostafiz.elasticlight.query.criteria.CriteriaQuery;
+import com.rimonmostafiz.elasticlight.query.criteria.TermQuery;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -10,7 +12,7 @@ import java.util.List;
  * @author Rimon Mostafiz
  */
 @Data
-public class Must {
+public class Must implements BooleanQuery {
     private List<CriteriaQuery> must;
 
     public Must() {
@@ -20,5 +22,15 @@ public class Must {
     public Must(List<CriteriaQuery> criteriaQueries) {
         this();
         this.must.addAll(criteriaQueries);
+    }
+
+    public Must addTerm(TermQuery termQuery) {
+        this.must.add(termQuery);
+        return this;
+    }
+
+    public Must addTerm(TermQueryBuilder termQueryBuilder) {
+        TermQuery termQuery = termQueryBuilder.build();
+        return this.addTerm(termQuery);
     }
 }
