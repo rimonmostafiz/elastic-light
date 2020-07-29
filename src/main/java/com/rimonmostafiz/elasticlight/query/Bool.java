4f60397c12872b0d6d2aaa1edc71c45b5c5b6601
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-07-29 14:33:10 +0600

    Add constructor and construction logic for bool and query

--- src/main/java/com/rimonmostafiz/elasticlight/query/Bool.java
+++ src/main/java/com/rimonmostafiz/elasticlight/query/Bool.java
@@ -1,14 +1,34 @@
 package com.rimonmostafiz.elasticlight.query;
 
 import com.rimonmostafiz.elasticlight.query.bool.BooleanQuery;
+import com.rimonmostafiz.elasticlight.query.bool.Must;
+import com.rimonmostafiz.elasticlight.query.bool.Should;
+import lombok.Data;
 
 /**
  * @author Rimon Mostafiz
  */
+@Data
 public class Bool {
     BooleanQuery bool;
 
     public Bool(BooleanQuery booleanQuery) {
         this.bool = booleanQuery;
+    }
+
+    public Bool(Must must) {
+        if (this.bool == null) this.bool = must;
+    }
+
+    public Bool(Should should) {
+        if (this.bool == null) this.bool = should;
+    }
+
+    public static Bool withMust(Must must) {
+        return new Bool(must);
+    }
+
+    public static Bool withShould(Should should) {
+        return new Bool(should);
     }
 }
--- src/main/java/com/rimonmostafiz/elasticlight/query/Query.java
+++ src/main/java/com/rimonmostafiz/elasticlight/query/Query.java
@@ -9,6 +9,7 @@ import lombok.Data;
 public class Query {
     private Bool query;
 
-    public Query() {
+    public Query(Bool bool) {
+        this.query = bool;
     }
 }
