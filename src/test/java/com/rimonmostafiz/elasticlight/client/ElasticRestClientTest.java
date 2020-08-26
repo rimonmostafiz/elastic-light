e566b8ce13797a53c49e1b768478f5f61b1f324b
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-08-26 12:09:26 +0600

    Migrate to Gradle project

--- src/test/java/com/rimonmostafiz/elasticlight/client/ElasticRestClientTest.java
+++ src/test/java/com/rimonmostafiz/elasticlight/client/ElasticRestClientTest.java
@@ -1,10 +1,14 @@
 package com.rimonmostafiz.elasticlight.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author Rimon Mostafiz
  */
+@Slf4j
+@SpringBootTest
 class ElasticRestClientTest {
 
     @Test
---
+++ src/test/java/com/rimonmostafiz/elasticlight/query/builder/TermQueryBuilderTest.java
@@ -0,0 +1,23 @@
+package com.rimonmostafiz.elasticlight.query.builder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Rimon Mostafiz
+ */
+@SpringBootTest
+class TermQueryBuilderTest {
+
+    private final String field = "";
+    private final String value = "";
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    void test() {
+
+    }
+}
